c974e8a7ef8eca6fd9defd93926d6cc89da84e5b
j3t1x <j3t1x@seznam.cz>
2024-11-27 01:44:24 +0100

    User Dao + User Schema

--- server/dao/user-dao.js
+++ server/dao/user-dao.js
@@ -25,6 +25,57 @@ class userDao {
       throw { code: "failedToCreateUser", message: error.message };
     }
   }
+
+  static async wishlistByUserId(id) {
+    try {
+      const user = await User.findById(id, "wishlist_array");
+      return user ? user.wishlist_array : [];
+    } catch (error) {
+      throw { code: "failedToGetWishlist", message: error.message };
+    }
+  }
+
+  static async cartByUserId(id) {
+    try {
+      const user = await User.findById(id, "cart_array");
+      return user ? user.cart_array : [];
+    } catch (error) {
+      throw { code: "failedToGetCart", message: error.message };
+    }
+  }
+
+  static async searchByFilter(filters) {
+    try {
+      return await User.find(filters);
+    } catch (error) {
+      throw { code: "failedToSearchUsers", message: error.message };
+    }
+  }
+
+  static async update(id, updatedData) {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        id,
+        { $set: updatedData },
+        { new: true, runValidators: true }
+      );
+      if (!updatedUser) {
+        throw { code: "failedToUpdateUser", message: "User not found." };
+      }
+      return updatedUser;
+    } catch (error) {
+      throw { code: "failedToUpdateUser", message: error.message };
+    }
+  }
+
+  static async list() {
+    try {
+      return await User.find({});
+    } catch (error) {
+      throw { code: "failedToListUsers", message: error.message };
+    }
+  }
+  
 }
 
 export default userDao;
