19741fe7c223d4a0e1ea3bba79aa79a997e9b332
MikulasNovak <miki.novak5@gmail.com>
2024-11-28 12:12:41 +0100

    profile

--- client/src/providers/UserProvider.js
+++ client/src/providers/UserProvider.js
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 export const UserContext = createContext();
 
 function UserProvider({ children }) {
-  const [user, setUser] = useState();
+  // const [user, setUser] = useState();
 
   async function handleRegister(user) {
     try {
@@ -66,12 +66,30 @@ function UserProvider({ children }) {
     }
   }
 
+  // Mock user data
+  const [user, setUser] = useState({
+    first_name: "Karel",
+    last_name: "Mácha",
+    email: "karel.macha@example.com",
+    phone_number: "123-456-7890",
+    dob: "1990-01-01",
+    role: "admin"
+  });
+
+  const updateUserProfile = (userData) => {
+    setUser(prevUser => ({
+      ...prevUser,
+      ...userData
+    }));
+  };
+
   const value = {
     user,
     handlerMap: {
       handleRegister,
       handleLogin,
       handleGoogleLogin,
+      updateUserProfile
     },
   };
 
