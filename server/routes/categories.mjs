ca42f8e763624f13c62e97e2aa4da6bce1a2a9f4
c1op <c1op@proton.me>
2024-11-30 18:58:58 +0100

    feat: add tree and subcategoires endpoints

--- server/routes/categories.mjs
+++ server/routes/categories.mjs
@@ -8,5 +8,7 @@ router.get("/", CategoryController.list);
 router.get(":id", CategoryController.get);
 router.put(":id", CategoryController.update);
 router.delete(":id", CategoryController.delete);
+router.get("/:categoryId/categories", CategoryController.getSubcategories);
+router.get("/:categoryId/tree", CategoryController.getCategoryTree);
 
 export default router;
