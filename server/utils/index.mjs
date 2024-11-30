4aae8c36305b69149d02564705143707dc73aa22
j3t1x <j3t1x@seznam.cz>
2024-11-30 01:17:01 +0100

    User fix

--- server/utils/index.mjs
+++ server/utils/index.mjs
@@ -1,5 +1,7 @@
 import { ApiError } from "./error.mjs";
 
+export { ApiError }; // Re-export ApiError
+
 export const requireParam = (value, params) => {
   if (!params[value]) {
     throw ApiError.badRequest(`${value} is a required search param`);
