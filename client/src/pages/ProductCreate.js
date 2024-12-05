cb17372c15f52d35b8aab33a3dbfae840220525b
MikulasNovak <miki.novak5@gmail.com>
2024-12-05 15:06:09 +0100

    Update route

--- client/src/pages/ProductCreate.js
+++ client/src/pages/ProductCreate.js
@@ -4,21 +4,8 @@ function ProductCreate() {
   return (
     <section className="profileContent">
       <ProfileSidebar />
-      <div>
-        <ProductForm />
-        <ProductForm
-          initialData={{ name: "John", price: "20CZK", description: "bla bal bla", isOnline: true, category: "category" }}
-          onSubmit={(data) => {
-            if (data.id) {
-              // Update logic
-              console.log("Updating:", data);
-            } else {
-              // Create logic
-              console.log("Creating:", data);
-            }
-          }}
-        />
-      </div>
+
+      <ProductForm />
 
     </section>
   );
