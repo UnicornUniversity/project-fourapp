0f698d637d402cb6eb7d7c147990155c28a7695a
j3t1x <j3t1x@seznam.cz>
2024-12-05 13:44:53 +0100

    Update index.mjs

--- server/utils/index.mjs
+++ server/utils/index.mjs
@@ -3,9 +3,10 @@ import { ApiError } from "./error.mjs";
 export { ApiError }; // Re-export ApiError
 
 export const requireParam = (value, params) => {
+  const paramValue = params[value];
   if (!params[value]) {
     throw ApiError.badRequest(`${value} is a required search param`);
   }
 
-  return params[value]; // Correctly access the key
+  return paramValue;
 };
