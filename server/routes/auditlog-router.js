cc781489788524bad1884835e3c87307f08246fd
Rinweros <110058210+Rinweros@users.noreply.github.com>
2024-12-15 11:44:03 +0100

    Přidán Paging.

--- server/routes/auditlog-router.js
+++ server/routes/auditlog-router.js
@@ -4,7 +4,7 @@ import AuditLogController from "../api/controllers/auditlog-controller.js";
 const router = express.Router();
 
 router.post("/", AuditLogController.create);
-router.get("/", AuditLogController.list);
+router.get("/", AuditLogController.list); // Podpora query parametrů pro stránkování
 router.get("/by-user/:userId", AuditLogController.listByUserId);
 router.get("/by-type-and-id", AuditLogController.listByTypeAndId);
 router.get("/by-status", AuditLogController.listByStatus);
