4b27a9632e6509f3b4663f5de781c0cf3f613a62
c1op <c1op@proton.me>
2024-12-19 13:13:49 +0100

    fix: orders and users controllers

--- server/routes/orders.mjs
+++ server/routes/orders.mjs
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import OrderController from "../api/controllers/order-controller.js";
+import { OrderController } from "../api/controllers/order-controller.js";
 
 const router = new Router();
 
-router.post("/", OrderController.create);
-router.post("/:id/complete", OrderController.complete);
-router.patch("/:id/payment", OrderController.addPaymentMethod);
-router.get("/filter", OrderController.filterByUser);
 router.get("/", OrderController.list);
+router.post("/", OrderController.create);
+router.delete("/:id", OrderController.deleteOrder);
+router.post("/:id/complete", OrderController.completeOrder);
+router.patch("/:id/payment", OrderController.addPaymentMethod);
+
 export default router;
