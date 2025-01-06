0c0a1c54248d129f9d3855bbf117afedbda287f5
c1op <c1op@proton.me>
2025-01-06 22:18:22 +0100

    fix: https

--- server/server.mjs
+++ server/server.mjs
@@ -35,7 +35,7 @@ app.use(cookieParser());
 app.use(requestLogger);
 app.use(auditRoute);
 
-app.use('/images', express.static(path.join(__dirname, 'assets/images')));
+app.use("/images", express.static(path.join(__dirname, "assets/images")));
 
 // Připojení k databázi
 try {
@@ -47,12 +47,12 @@ try {
 }
 
 // Endpointy
-app.use("/api/auth", authRouter);
-app.use("/api/categories", categoriesRouter);
-app.use("/api/products", productsRouter);
-app.use("/api/users", userRouter);
-app.use("/api/orders", ordersRouter);
-app.use("/api/auditlogs", auditlogRouter);
+app.use("auth", authRouter);
+app.use("categories", categoriesRouter);
+app.use("products", productsRouter);
+app.use("users", userRouter);
+app.use("orders", ordersRouter);
+app.use("auditlogs", auditlogRouter);
 app.use(errorHandler);
 
 // Spuštění serveru
