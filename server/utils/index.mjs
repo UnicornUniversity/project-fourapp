b33138280a00396f8a5f7d315ab2bf359914a927
j3t1x <j3t1x@seznam.cz>
2025-01-07 13:44:51 +0100

    branch fix

--- server/utils/index.mjs
+++ server/utils/index.mjs
@@ -3,10 +3,9 @@ import { ApiError } from "./error.mjs";
 export { ApiError }; // Re-export ApiError
 
 export const requireParam = (value, params) => {
-  const paramValue = params[value];
   if (!params[value]) {
     throw ApiError.badRequest(`${value} is a required search param`);
   }
 
-  return paramValue;
+  return params[value]; // Correctly access the key
 };
